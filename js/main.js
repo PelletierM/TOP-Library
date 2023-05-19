c96f63822ac20ad5daba2f483d8462a3326e66b4
PelletierM <122843454+PelletierM@users.noreply.github.com>
2023-05-19 16:39:59 -0400

    Complete form styling and improve JS
    JS: Remove variables from global scope

--- js/main.js
+++ js/main.js
@@ -1,144 +1,155 @@
-let bookList = [];
+let addBook;
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.delete = {};
-  this.index = (bookList.length); /* Index given before book is added to bookList */
-}
+/* Book list init */
+(() => {
+  let bookList = [];
 
-Book.prototype.toggleReadMethod = function toggleReadMethod() { this.read = !this.read; };
-
-function createBookElement(key, value) {
-  const element = document.createElement('div');
-  element.classList.add(key);
-  element.appendChild(document.createTextNode(value));
-  return element;
-}
-
-function createBookDOM(book, dataIndex) {
-  let bookDOM = document.createElement('div');
-  Object.keys(book).forEach((key) => {
-    if (typeof book[key] === 'string') {
-      bookDOM.appendChild(createBookElement(key, book[key]));
-    } else {
-      bookDOM.appendChild(createBookElement(key, ''));
-    }
-  });
-  bookDOM.dataset.index = dataIndex;
-  bookDOM.classList.add(`read-${book.read}`);
-  const parent = document.querySelector('.book-list');
-  parent.appendChild(bookDOM);
-}
-
-function addBook(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-  bookList.push(book);
-  createBookDOM(book, book.index);
-}
-
-function updateIndexes() {
-  bookList.forEach((book) => {
-    const previousIndex = book.index;
-    // eslint-disable-next-line no-param-reassign
-    book.index = bookList.indexOf(book);
-    const bookDOM = document.querySelector(`[data-index='${previousIndex}']`);
-    bookDOM.dataset.index = book.index;
-  });
-}
-
-function toggleRead(e) {
-  const parentIndex = e.target.parentNode.dataset.index;
-  bookList[parentIndex].toggleReadMethod();
-  e.target.parentNode.classList.remove(`read-${!bookList[parentIndex].read}`);
-  e.target.parentNode.classList.add(`read-${bookList[parentIndex].read}`);
-}
-
-function deleteElement(e) {
-  const parentIndex = e.target.parentNode.dataset.index;
-  bookList.splice(parentIndex, 1);
-  e.target.parentNode.remove();
-  updateIndexes();
-}
-
-document.addEventListener('click', (e) => {
-  if (e.target.classList.contains('read')) {
-    toggleRead(e);
+  function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.delete = {};
+    this.index = (bookList.length); /* Index given before book is added to bookList */
   }
-  if (e.target.classList.contains('delete')) {
-    deleteElement(e);
+
+  Book.prototype.toggleReadMethod = function toggleReadMethod() { this.read = !this.read; };
+
+  function createBookElement(key, value) {
+    const element = document.createElement('div');
+    element.classList.add(key);
+    element.appendChild(document.createTextNode(value));
+    return element;
   }
-});
 
-/* Form interactions and validation */
-const inputFields = document.querySelectorAll('input');
-const showModal = document.querySelector('.show-form');
-const closeModalButton = document.querySelector('.close-modal');
+  function createBookDOM(book, dataIndex) {
+    let bookDOM = document.createElement('div');
+    Object.keys(book).forEach((key) => {
+      if (typeof book[key] === 'string') {
+        bookDOM.appendChild(createBookElement(key, book[key]));
+      } else {
+        bookDOM.appendChild(createBookElement(key, ''));
+      }
+    });
+    bookDOM.dataset.index = dataIndex;
+    bookDOM.classList.add(`read-${book.read}`);
+    const parent = document.querySelector('.book-list');
+    parent.appendChild(bookDOM);
+  }
 
-showModal.addEventListener('click', () => {
-  const modal = document.querySelector('.modal-form');
-  modal.classList.add('active');
-});
+  addBook = (title, author, pages, read) => {
+    const book = new Book(title, author, pages, read);
+    bookList.push(book);
+    createBookDOM(book, book.index);
+    console.log(bookList);
+  };
 
-function resetForm() {
-  document.querySelector('form').reset();
-  inputFields.forEach((inputField) => {
-    inputField.parentElement.classList.remove('invalid');
-    inputField.parentElement.querySelector('label').classList.remove('active');
-  });
-}
+  function updateIndexes() {
+    bookList.forEach((book) => {
+      const previousIndex = book.index;
+      // eslint-disable-next-line no-param-reassign
+      book.index = bookList.indexOf(book);
+      const bookDOM = document.querySelector(`[data-index='${previousIndex}']`);
+      bookDOM.dataset.index = book.index;
+    });
+  }
 
-function closeModal() {
-  const modal = document.querySelector('.modal-form');
-  modal.classList.remove('active');
-  resetForm();
-}
+  function toggleRead(e) {
+    const parentIndex = e.target.parentNode.dataset.index;
+    bookList[parentIndex].toggleReadMethod();
+    e.target.parentNode.classList.remove(`read-${!bookList[parentIndex].read}`);
+    e.target.parentNode.classList.add(`read-${bookList[parentIndex].read}`);
+  }
 
-closeModalButton.addEventListener('click', () => {
-  closeModal();
-});
+  function deleteElement(e) {
+    const parentIndex = e.target.parentNode.dataset.index;
+    bookList.splice(parentIndex, 1);
+    e.target.parentNode.remove();
+    updateIndexes();
+  }
 
-inputFields.forEach((inputField) => {
-  inputField.addEventListener('focusin', (e) => {
-    e.target.parentElement.classList.add('focus');
-    e.target.parentElement.querySelector('label').classList.add('active');
-  });
-  inputField.addEventListener('focusout', (e) => {
-    e.target.parentElement.classList.remove('focus');
-    if (e.target.value === '') {
-      e.target.parentElement.querySelector('label').classList.remove('active');
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('read')) {
+      toggleRead(e);
+    }
+    if (e.target.classList.contains('delete')) {
+      deleteElement(e);
     }
   });
-  inputField.addEventListener('input', (e) => {
-    if (e.target.validity.valid) {
-      e.target.parentElement.classList.remove('invalid');
-    }
-  });
-});
+})();
 
-function checkFormValidity() {
-  let output = true;
-  inputFields.forEach((inputField) => {
-    if (!inputField.validity.valid) {
-      inputField.parentElement.classList.add('invalid');
-      output = false;
-    }
-  });
-  return output;
-}
+/* Form init */
+(() => {
+  const inputFields = document.querySelectorAll('input');
+  const showModal = document.querySelector('.show-form');
+  const closeModalButton = document.querySelector('.close-modal');
 
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-  if (checkFormValidity()) {
-    addBook(data.get('title'), data.get('author'), data.get('pages'), !!data.get('read'));
+  showModal.addEventListener('click', () => {
+    const modal = document.querySelector('.modal-form');
+    modal.classList.add('active');
+  });
+
+  function resetForm() {
+    document.querySelector('form').reset();
+    inputFields.forEach((inputField) => {
+      inputField.parentElement.classList.remove('invalid');
+      inputField.parentElement.querySelector('label').classList.remove('active');
+    });
+  }
+
+  function closeModal() {
+    const modal = document.querySelector('.modal-form');
+    modal.classList.remove('active');
+    setTimeout(() => resetForm(), getComputedStyle(document.querySelector(':root')).getPropertyValue('--transition-time-standard').replace(/\D/g, ''));
+  }
+
+  closeModalButton.addEventListener('click', () => {
     closeModal();
-  }
-});
+  });
 
-/* For testing - remove when done */
-addBook('Book 1', 'By Me', '287', true);
-addBook('Book 2', 'Not by me', '123', false);
+  inputFields.forEach((inputField) => {
+    inputField.addEventListener('focusin', (e) => {
+      e.target.parentElement.classList.add('focus');
+      e.target.parentElement.querySelector('label').classList.add('active');
+    });
+    inputField.addEventListener('focusout', (e) => {
+      e.target.parentElement.classList.remove('focus');
+      if (e.target.value === '') {
+        e.target.parentElement.querySelector('label').classList.remove('active');
+      }
+    });
+    inputField.addEventListener('input', (e) => {
+      if (e.target.validity.valid) {
+        if (e.target.type === 'radio') {
+          const radioDivs = e.target.parentElement.parentElement.querySelectorAll('div');
+          radioDivs.forEach((radioDiv) => { radioDiv.classList.remove('invalid'); });
+        } else {
+          e.target.parentElement.classList.remove('invalid');
+        }
+      }
+    });
+  });
+
+  function checkFormValidity() {
+    let output = true;
+    inputFields.forEach((inputField) => {
+      if (!inputField.validity.valid) {
+        inputField.parentElement.classList.add('invalid');
+        output = false;
+      }
+    });
+    return output;
+  }
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(form);
+    if (checkFormValidity()) {
+      addBook(data.get('title'), data.get('author'), data.get('pages'), !!data.get('read'));
+      closeModal();
+    }
+  });
+})();
+
+addBook('The Hobbit', 'J.R.R. Tolkien', '317', true);
